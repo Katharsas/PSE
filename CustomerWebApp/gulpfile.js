3d477d5f415e60314f83fd326572ae366f02a3da
Jan <Katharsas@users.noreply.github.com>
2016-05-15 23:43:28 +0200

    CWA: Fixed #19 gulp typescript build
    - tested ts building and file watching
    - wrote Dev Guide on how to build typescript

--- CustomerWebApp/gulpfile.js
+++ CustomerWebApp/gulpfile.js
@@ -3,22 +3,29 @@ var ts = require('gulp-typescript');
 
 var tsSource = "./src/main/typescript/**/*.ts";
 var tsTarget = "./src/main/webapp/WEB-INF/resources/js";
+var tsTargetFile = "index.bundle.js";
 var tsOptions = {
-	module: "commonjs";//TODO build for browser (bundle), not for commonjs
+	target: "es5",
+	outFile: tsTargetFile,
+	noImplicitAny: true,
+	noExternalResolve : true
 }
 
 /**
  * Compiles all typescript files to javascript modules (as specified in tsOptions).
  */
 gulp.task('typescript', function() {
-    console.log('Compiling typescript');
+	var fullTarget = tsTarget + '/' + tsTargetFile
+    console.log('Compiling and bundling typescript to ' + fullTarget);
     return gulp.src([tsSource])
-        .pipe(ts(tsOptions)).js.pipe(gulp.dest(tsTarget));
+        .pipe(ts(tsOptions))
+		.pipe(gulp.dest(tsTarget));
 });
 
 /**
  * Watches all typescript files and recompiles on change.
  */
 gulp.task('watch', function() {
+	console.log('Watching files at ' + tsSource + ' for changes...');
     gulp.watch(tsSource, ['typescript']);
 });
