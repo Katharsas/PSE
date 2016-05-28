f1e0a02a138389cd6eec62d85840112f555fc605
SemperVideo <antonkolb@users.noreply.github.com>
2016-05-28 17:46:48 +0200

    ESController (kompilierbar) put Methoden, sind jedoch noch abstrakt kompilierbar!

--- ESFeeder/src/ElasticSearchWriter.java
+++ ESFeeder/src/ElasticSearchWriter.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.lang.Exception;
+import java.util.List;
 
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.node.NodeBuilder;
@@ -9,10 +10,13 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.admin.indices.flush.FlushRequest;
 
 /**
  *
  * @author akolb
+ * @author dbeckstein
  */
 
 public class ElasticSearchWriter extends ElasticSearchController{
@@ -28,30 +32,31 @@ public class ElasticSearchWriter extends ElasticSearchController{
 
 		CreateIndexRequestBuilder indexBuilder = client.admin().indices().prepareCreate(indexName);
 		indexBuilder.addMapping(indexType, jsonBuilder()
-                        .startObject()
-                            .startObject("properties")
-                                .startObject(obj_title)
-                                    .field("type","string")
-                                    .field("index", "analyzed")
-                                .endObject()
-                                .startObject(obj_pubDate)
-                                    .field("type","date")
-                                    .field("index", "analyzed")
-                                .endObject()
-                                .startObject(obj_content)
-                                    .field("type","string")
-                                    .field("index", "analyzed")
-                                .endObject()
-                                .startObject(obj_author)
-                                    .field("type","string")
-                                    .field("index", "analyzed")
-                                .endObject()
-                            .endObject()
-                        .endObject());
-                        
-        //executes and get's the response, which currently isn't used
+        		.startObject()
+                    .startObject("properties")
+                    	.startObject(obj_title)
+                    		.field("type","string")
+                            .field("index", "analyzed")
+                        .endObject()
+                        .startObject(obj_pubDate)
+                             .field("type","date")
+                             .field("index", "analyzed")
+                        .endObject()
+                        .startObject(obj_content)
+                             .field("type","string")
+                             .field("index", "analyzed")
+                        .endObject()
+                        .startObject(obj_author)
+                             .field("type","string")
+                             .field("index", "analyzed")
+                        .endObject()
+        			.endObject()
+        		.endObject()
+        );
+
+        //executes and get's the response
         CreateIndexResponse createResponse = indexBuilder.get();
-        
+
         //Check if index is created
         if (!createResponse.isAcknowledged()) {
             throw new Exception("Failed to create index <" + indexName + ">");
@@ -63,6 +68,45 @@ public class ElasticSearchWriter extends ElasticSearchController{
 
 
 	}
+	
+	/*
+	 * indexes an object in the ES db
+	 * jsonBuilder throws an IOException if an issue occurs
+	 */
+	private void put( Object o, String index ) throws IOException{
+	
+		//TODO: Muss aus Object rausgelesen werden
+		String id = "", title = "", pubDate = "", content = "", author = "";
+		
+		//get() executes and gets the response
+		IndexResponse indexResponse = client.prepareIndex(index, this.indexType, id)
+            .setSource(jsonBuilder()
+                .startObject()
+                    .field(obj_title, title)
+                    .field(obj_pubDate, pubDate)
+                    .field(obj_content, content)
+                    .field(obj_author, author)
+                .endObject()
+            ).get();
+            
+/*DEBUG*/String _index = indexResponse.getIndex(), _type = indexResponse.getType(), _id = indexResponse.getId();
+            // Version (if it's the first time you index this document, you will get: 1)
+            long _version = indexResponse.getVersion();
+            // isCreated() is true if the document is a new one, false if it has been updated
+            boolean created = indexResponse.isCreated();
+            System.out.println("Index: " + _index + " Type: " + _type + " ID: " + _id + " Version: " + _version + " Indexed(t) or Updated(f): " +created);
+	
+	}
+	
+	/**
+	 * is called whenever an Article should be indexed
+	 * identical or similair article?
+	 * if true -> don't add to DB
+	 */
+	private boolean articleIsAlreadyIndexed( Object o ){
+		
+		return false;
+	}
 
 	/**
 	 * Also creates indexes
@@ -71,7 +115,7 @@ public class ElasticSearchWriter extends ElasticSearchController{
 	public ElasticSearchWriter() throws IOException, Exception{
 
 		super();
-		
+
 		// org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse.isExists()
 		boolean mainIndex_exists =  client.admin().indices().prepareExists(mainIndex).get().isExists();
 		boolean searchIndex_exists =  client.admin().indices().prepareExists(searchIndex).get().isExists();
@@ -84,17 +128,45 @@ public class ElasticSearchWriter extends ElasticSearchController{
 		}
 	}
 
-	public void initialize(){}
-
 	/*
-	 * indexes an object in the ES db
-	 * needs the name and type of the created index and an ID for the object
+	 * puts an article in both indexes
+	 * passes the IOException from private put()
 	 */
-	public void put(){}
+	public void put( Object o ) throws IOException{
+	
+		this.put( o, mainIndex );
+		
+		//Should be already indexed if it is worth to us
+		if(!this.articleIsAlreadyIndexed( o )){
+			this.put( o, searchIndex );
+		}
+		
+		/*
+		makes sure, that no data is in the node
+		new FlushRequest(): no parameters, so all indices are flushed
+		force(true): forces to flush, even if it is not necessary
+		actionGet(): execute and return a org.elasticsearch.action.admin.indices.flush.FlushResponse which isn't used
+		*/
+		client.admin().indices().flush( new FlushRequest().force(true) ).actionGet();
+	
+	}
+	
+	/*
+	 * puts a list of articles in both indexes
+	 * passes the IOException from public put()
+	 */
+	public void putMany( List<Object> l ) throws IOException{
+	
+		for( Object o : l ){
+			this.put( o );
+		}
+	
+	}
 
 	/*
 	 * deletes an object from the index, but lets it stay in the db.
 	 * needs the name and type of the created index and an ID for the object
+	 * obsolete?
      */
 	public void delete(){}
 
@@ -119,7 +191,7 @@ public class ElasticSearchWriter extends ElasticSearchController{
 	public void getAll(){}
 
 	/**
-	 *
+	 * needed?
 	 *
 	 */
 	public void getSimilair(){}
