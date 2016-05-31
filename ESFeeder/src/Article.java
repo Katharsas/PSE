8fb8ea9c4161dac85298449b413b3d88cf7e94c8
SemperVideo <antonkolb@users.noreply.github.com>
2016-05-31 13:48:13 +0200

    ESController + XML-Stuff

---
+++ ESFeeder/src/Article.java
@@ -0,0 +1,20 @@
+
+
+public class Article{
+
+	private String id, //hashed article name
+			title,
+			pubDate, //"2000-12-11"
+			extractedText,
+			author = "",
+			topic, //buisiness, culture
+			source, //Spiegel, CNN
+			url; //link to article
+			
+	//metainfos can be placed here if needed
+	private String[] meta;
+
+	public Article(){}
+
+
+}
