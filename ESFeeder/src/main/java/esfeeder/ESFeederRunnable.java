52333f0b472610001cf49af4a69e5d650cbd4f4a
SemperVideo <antonkolb@users.noreply.github.com>
2016-05-31 16:21:15 +0200

    K ES Stuff
    Exclusiver Push für DoubleB

--- ESFeeder/src/main/java/esfeeder/ESFeederRunnable.java
+++ ESFeeder/src/main/java/esfeeder/ESFeederRunnable.java
@@ -2,23 +2,29 @@ package esfeeder;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ *
+ * @author jmothes
+ */
+ 
 public class ESFeederRunnable implements Runnable {
 
 	private NewArticleManager newArticleManager;
 	private DuplicateManager duplicateManager;
-	
+
 	@Override
 	public void run() {
 		newArticleManager = new NewArticleManager();
 		List<ArticleId> newIds = newArticleManager.manageArticles();
 		duplicateManager = new DuplicateManager();
-		duplicateManager.manageDuplicates(newIds);
 	}
-	
-	
+
+
 	public static void main(String[] args) {
 		System.out.println("Executing ESFeeder...");
 		FileService fileService = new FileService();
 		List<Path> newArticlePaths = fileService.getSubscribedArticles(false);
+		
+
 	}
 }
--- ESFeeder/src/main/java/esfeeder/FileService.java
+++ ESFeeder/src/main/java/esfeeder/FileService.java
@@ -21,47 +21,53 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
+/**
+ *
+ * @author jmothes
+ * @author akolb
+ */
+
 public class FileService {
-	
+
 	private final static Charset encoding = StandardCharsets.UTF_8;
-	
+
 	private final static Path notificationFolder = Paths.get("./notifications");
 	private final static String notificationFilePrefix = "notification_";
-	
+
 	private final static Path archive = Paths.get("./../RSSCrawler/archive_dev");
-	
+
 	/**
 	 * Reads in every line of every notification file in the notification folder.
 	 * Converts the lines to paths and returns them.
 	 * Notification fileNames must start with {@link #notificationFilePrefix}.
-	 * 
-	 * @param deleteNotificationFiles - If true, 
+	 *
+	 * @param deleteNotificationFiles - If true,
 	 * @return - All lines as a list of Path objects.
 	 */
 	public List<Path> getSubscribedArticles(boolean deleteNotificationFiles) {
 		try {
-			
+
 			List<Path> notificationFiles = Files.walk(notificationFolder)
 				.filter(Files::isRegularFile)
 				.filter(path -> path.getFileName().toString().startsWith(notificationFilePrefix))
 				.collect(Collectors.toList());
-			
+
 			List<Path> newArticles = notificationFiles.stream()
 				.flatMap(getLines)
 				.map(articlePathString -> Paths.get(articlePathString))
 				.collect(Collectors.toList());
-			
+
 			if (deleteNotificationFiles) {
 				notificationFiles.stream()
 					.forEach(notificationFile -> notificationFile.toFile().delete());
 			}
 			return newArticles;
-			
+
 		} catch (IOException e) {
 			throw new UncheckedIOException(e);
 		}
 	}
-	
+
 	/**
 	 * Converts a path to a stream of all lines in that file.
 	 */
@@ -72,19 +78,19 @@ public class FileService {
 			throw new UncheckedIOException(e);
 		}
 	};
-	
+
 	public Map<Path, Document> getArticles(List<Path> articlePaths) {
 		try {
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			
+
 			Map<Path, Document> result = new HashMap<>();
 			for(Path articlePath : articlePaths) {
 				Document articleXml = dBuilder.parse(archive.resolve(articlePath).toFile());
 				result.put(articlePath, articleXml);
 			}
 			return result;
-			
+
 		} catch (ParserConfigurationException | SAXException e) {
 			throw new RuntimeException(e);
 		} catch (IOException e) {
