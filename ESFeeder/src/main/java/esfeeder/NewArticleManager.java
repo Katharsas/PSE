6b49db307df87763a12c79233f221339b4c4f031
Philipp <pmarek2401@users.noreply.github.com>
2016-06-01 12:33:39 +0200

    Angepasst: ESFeeder, Methodenstümpfe hinzugefügt

--- ESFeeder/src/main/java/esfeeder/NewArticleManager.java
+++ ESFeeder/src/main/java/esfeeder/NewArticleManager.java
@@ -1,9 +1,42 @@
 package esfeeder;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
+
+import org.w3c.dom.Document;
+
+import elasticsearch.ElasticSearchController;
+import elasticsearch.ElasticSearchWriter;
 
 public class NewArticleManager {
-
+	private ElasticSearchController eswriter;
+	private FileService fileservice;
+	
+	public NewArticleManager(){
+		try {
+			eswriter = new ElasticSearchWriter();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		fileservice = new FileService();
+	}
+	
 	public List<ArticleId> manageArticles() {
 		throw new UnsupportedOperationException("Not yet implemented!");
 	}
+	
+	//TODO Replace Object with some kind of Json-Object
+	public List<ArticleId> addArticles(List<Object> json){
+		throw new UnsupportedOperationException("Not yet implemented!");
+	}
+	
+	public void parseToJson(Map<Path, Document> xml){
+		throw new UnsupportedOperationException("Not yet implemented!");
+	}
+	
 }
