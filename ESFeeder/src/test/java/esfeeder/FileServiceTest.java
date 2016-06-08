7994ca20df43bb3aa16c89087b69df771b687294
Philipp <pmarek2401@users.noreply.github.com>
2016-06-08 17:02:06 +0200

    ESFeeder: Angelegt: Kommentar-Stubs für ArticleParser, Angelegt: Testklassen für ESFeeder

---
+++ ESFeeder/src/test/java/esfeeder/FileServiceTest.java
@@ -0,0 +1,44 @@
+package esfeeder;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FileServiceTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetSubscribedArticles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetArticles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetArticles_debug() {
+		fail("Not yet implemented");
+	}
+
+}
