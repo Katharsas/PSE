b0269fa8fbd36cdffbdcd4766ae1f8cb37e876fe
Johannes Franz <jofranz90@gmail.com>
2016-06-08 17:16:21 +0200

    Teststruktur der Methoden für Elasticsearch

---
+++ ESFeeder/src/test/java/elasticsearch/ElasticSearchControllerTest.java
@@ -0,0 +1,52 @@
+package elasticsearch;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ElasticSearchControllerTest
+{
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void testElasticSearchController()
+    {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFree()
+    {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMetaDataFromIndex()
+    {
+	fail("Not yet implemented");
+    }
+
+}
