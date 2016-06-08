71616caa3c472b7f82f3ee9cce06296a278bdb92
Johannes Franz <jofranz90@gmail.com>
2016-06-08 15:29:13 +0200

    Dokument aktualisiert und Test-Test

---
+++ CustomerWebApp/src/main/java/cwa/controller/HelloWorldPageControllerTest.java
@@ -0,0 +1,52 @@
+package cwa.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HelloWorldPageControllerTest
+{
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public final void testSerachPage()
+    {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testIndexPage()
+    {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testParse_post()
+    {
+	fail("Not yet implemented");
+    }
+
+}
