85e577dc5b803023e54d6881a73b3067833c4637
Philipp <pmarek2401@users.noreply.github.com>
2016-06-14 15:13:55 +0200

    ESFeeder: Comments & Testklassen erweitert

--- ESFeeder/src/test/java/esfeeder/XmlParserTest.java
+++ ESFeeder/src/test/java/esfeeder/XmlParserTest.java
@@ -2,7 +2,11 @@ package esfeeder;
 
 import static org.junit.Assert.fail;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,7 +14,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class XmlParserTest {
-	
+
 	XmlParser xmlParser;
 
 	@BeforeClass
@@ -31,28 +35,65 @@ public class XmlParserTest {
 	}
 
 	@Test
+	public void testParse_xml() {
+		String returnValue;
+		Class targetClass = xmlParser.getClass();
+		Class[] cArg = new Class[2];
+		cArg[0] = Element.class;
+		cArg[1] = String.class;
+		Method method;
+		
+		try {
+			method = targetClass.getDeclaredMethod("parse_xml", cArg);
+			method.setAccessible(true);
+
+			method.invoke(targetClass, cArg);
+			fail("Should throw NullPointer");
+		} catch (NoSuchMethodException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SecurityException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InvocationTargetException e) {
+
+		} catch (IllegalAccessException e) {
+		} catch (NullPointerException e) {
+
+		}
+
+		try {
+
+		} catch (NullPointerException e) {
+
+		} catch (Exception e) {
+
+		}
+	}
+
+	@Test
 	public void testParse() {
 		Document doc = null;
-		try{
+		try {
 			xmlParser.parse(null, null);
 			fail("Should throw Nullpointer");
 			xmlParser.parse(null, doc);
 			fail("Should throw Nullpointer");
 			xmlParser.parse(null, null);
 			fail("Should throw Nullpointer");
-		} catch (NullPointerException e){
-			
+		} catch (NullPointerException e) {
+
 		}
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testParseFileList() {
-		try{
+		try {
 			xmlParser.parseFileList(null);
 			fail("Should throw Nullpointer");
-		} catch (NullPointerException e){
-			
+		} catch (NullPointerException e) {
+
 		}
 		fail("Not yet implemented");
 	}
