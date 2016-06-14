473f3a95ee099f560af2c57c87fa13f17ac16da1
Philipp <pmarek2401@users.noreply.github.com>
2016-06-14 11:06:52 +0200

    ESFeeder: Testklasse ArticleParserTest erweitert -> Bitte Fehler korrigieren (@dbeckstein)

--- ESFeeder/src/test/java/esfeeder/ArticleParserTest.java
+++ ESFeeder/src/test/java/esfeeder/ArticleParserTest.java
@@ -1,7 +1,9 @@
 package esfeeder;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import org.joda.time.IllegalFieldValueException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,8 +12,10 @@ import org.junit.Test;
 
 public class ArticleParserTest {
 
+	ArticleParser articleParser;
+	
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() throws Exception {	
 	}
 
 	@AfterClass
@@ -20,20 +24,57 @@ public class ArticleParserTest {
 
 	@Before
 	public void setUp() throws Exception {
+		articleParser = new ArticleParser();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		articleParser = null;
 	}
 
 	@Test
 	public void testParse_pubDate() {
-		fail("Not yet implemented");
+		String returnValue;
+		try{
+			articleParser.parse_pubDate(null);
+			fail("Should throw Nullpointer");
+		} catch (NullPointerException e){
+		}
+		
+		returnValue = articleParser.parse_pubDate("");
+		assertEquals("Empty String not catched!", "", returnValue);
+		
+		returnValue = articleParser.parse_pubDate("Sat, 12 Nov 2011 13:02:53 EST");
+		assertEquals("Implementation incorrect","2011 11 12 13:02:53",returnValue);
+		
+		returnValue = articleParser.parse_pubDate("Tue, 14 Jun 2016 10:50:20 EST");
+		assertEquals("Implementation incorrect", "2016 06 14 10:50:20", returnValue);
+		
+		try{
+			articleParser.parse_pubDate("SomeRandomCharaktersAndStuffYouSeeEveryDayAnItIsPrettyLong");
+			fail("Should throw something");
+			articleParser.parse_pubDate("Tes");
+			fail("Should throw something");
+			articleParser.parse_pubDate("123");
+			fail("Should throw something");
+			articleParser.parse_pubDate("XYZ, 40 Fex 0001 99:99:99 ECT");
+			fail("Should throw something");
+		} catch (Exception e){
+			
+		}
 	}
 
 	@Test
 	public void testParse_source() {
-		fail("Not yet implemented");
+		String returnValue;
+		returnValue = articleParser.parse_source("http://blog.google.com/hello/devblog/info.html");
+		assertEquals("Implementation incorrect", "google.com", returnValue);
+		returnValue = articleParser.parse_source("");
+		assertEquals("Empty String not catched!", "", returnValue);
+		returnValue = articleParser.parse_source("https://www.google.de");
+		assertEquals("Implementation incomplete", "google.de", returnValue);
+		returnValue = articleParser.parse_source("http://blog.tumblr.co.uk/index.html");
+		assertEquals("Implementation incomplete", "tumblr.co.uk", returnValue);
 	}
 
 	@Test
