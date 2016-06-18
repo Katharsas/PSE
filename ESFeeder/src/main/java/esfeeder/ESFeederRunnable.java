b715402491690779221e5c271d6fbd9f198ce070
SemperVideo <antonkolb@users.noreply.github.com>
2016-06-18 18:21:42 +0200

    K ElasticSearchController Upps...

--- ESFeeder/src/main/java/esfeeder/ESFeederRunnable.java
+++ ESFeeder/src/main/java/esfeeder/ESFeederRunnable.java
@@ -15,8 +15,8 @@ import org.w3c.dom.Document;
 
 import shared.Article;
 import elasticsearch.ElasticSearchWriter;
- 
- 
+
+
 public class ESFeederRunnable implements Runnable {
 
     @Override
@@ -34,12 +34,12 @@ System.out.println("Executing ESFeeder..."); //DEBUG
     	FileService fileService = new FileService();
     	XmlParser xmlParser = new XmlParser();
     	ElasticSearchWriter esWriter;
-    	
+
 		//Returns a Map with Path to Document; false -> don't delete Notification Files
 		Map<Path, Document> newArticlePaths = fileService.getSubscribedArticles(false);
 		//Uses the Map and returns an ArrayList with Articles
 		List<Article> newArticles = xmlParser.parseFileList(newArticlePaths);
-    	
+
     	try{
     		esWriter = new ElasticSearchWriter();
     		try{
@@ -47,14 +47,16 @@ System.out.println("Executing ESFeeder..."); //DEBUG
 			}catch(IOException putArticle){
 				//Puts the Articles from the ArrayList in ES
 				putArticle.printStackTrace();
+				//last call to esWriter that can and must be done! esWriter == destroyed
+				esWriter.free();
 			}
     	}catch(IOException createESW){
     		createESW.printStackTrace();
     	}catch(Exception e){
     		e.printStackTrace();
     	}
-		
-		
+
+
 
         /*
         * Daniel's code
