9753ddaa4209c1df9ed47806591e709e23aa03aa
DanielBeckstein <DanielBeckstein@users.noreply.github.com>
2016-07-03 12:13:13 +0200

    CWA: buggy js

--- CustomerWebApp/src/main/typescript/main.ts
+++ CustomerWebApp/src/main/typescript/main.ts
@@ -139,7 +139,7 @@
         list.innerHTML = "";
         var sample = document.getElementById("result_sample");
 
-
+        
         for (var i = 0; i < 0; i++) { //bug
             var el = sample.cloneNode(true); // bug overwritten by ts
             list.appendChild(el);
@@ -254,8 +254,37 @@
         
         function f_search_similar(el : any){
             console.log("------similar----",el);
-            var id  = el.getAttribute('data-articleId');
-            console.log(id);
+            var articleId  = el.getAttribute('data-articleId');
+            console.log(articleId);
+              /*  
+            var list = document.getElementById("result_sample_list");
+            list.innerHTML = "";
+            var sample = document.getElementById("result_sample");
+            
+            var cs_log_ajax_hint = "___similar___";
+            
+               Ajax.getBySimilar(articleId, 0, 10)
+                    .done(function(result: ArticleResult) {
+                        if (result.errorMessage !== null) {
+                            console.log(cs_log_ajax_hint, result.errorMessage);
+                        } else {
+                            console.log(cs_log_ajax_hint, "Articles received:");
+                            for (let article of result.articles) {
+                                console.log(cs_log_ajax_hint, article);
+                                console.log(article.author);
+
+                                var list = <Node> document.getElementById("result_sample_list");
+                                //var sample = (<Node> document.getElementById("result_sample") );
+                                console.log(list);
+                                HtmlBuilder.buildArticle(article, list);
+                            }
+                        }
+                    })
+                    .fail(function() {
+                        console.log(cs_log_ajax_hint, "Sending request failed!");
+                    });
+                
+                */
         }
         
         function f_date_set_range(el : any){
